d951a70687d397c2de4d74f45085f2e77bf37a11
KalapacsJoco <kalapacs.joco79@gmail.com>
2025-01-28 11:27:04 +0200

    before messing up

--- app/Filament/Resources/AnimalResource/Pages/ListAnimals.php
+++ app/Filament/Resources/AnimalResource/Pages/ListAnimals.php
@@ -29,7 +29,7 @@ class ListAnimals extends ListRecords
                 //
             ])
             ->actions([
-                ViewAction::make()
+
             ]);
     }
 
--- app/Filament/Resources/AnimalResource/Pages/ViewAnimal.php
+++ app/Filament/Resources/AnimalResource/Pages/ViewAnimal.php
@@ -3,9 +3,28 @@
 namespace App\Filament\Resources\AnimalResource\Pages;
 
 use App\Filament\Resources\AnimalResource;
+use App\Models\Image;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewAnimal extends ViewRecord
 {
     protected static string $resource = AnimalResource::class;
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                TextEntry::make('name')->label('Name:'),
+                TextEntry::make('age')->label('Age:'),
+                TextEntry::make('color')->label('Color:'),
+                TextEntry::make('sex')->label('Sex:'),
+                TextEntry::make('status')->label('Status:'),
+            ]);
+    }
 }
