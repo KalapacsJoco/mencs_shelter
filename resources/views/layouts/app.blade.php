6150fd57ee846f3f8311b49698331c9056d6be98
KalapacsJoco <kalapacs.joco79@gmail.com>
2025-02-03 16:18:06 +0200

    wprk started

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,34 @@
 
 <body class="font-sans antialiased">
     <nav>
+        <a href="/"> <img src="{{ asset('storage/logo/logo.png') }}" alt="Logo">
+        </a>
+        <div x-data="{ 
+            activeIndex: 0, 
+            links: [
+                { text: 'Adopt now', url: '#' },
+                { text: 'Find Vets', url: '#' },
+                { text: 'Hostels', url: '#' }
+            ] 
+        }" x-init="setInterval(() => activeIndex = (activeIndex + 1) % links.length, 3000)"
+            class="flex flex-col items-center space-y-4">
+
+            <ul class="flex space-x-4 text-lg font-semibold">
+                <template x-for="(link, index) in links" :key="index">
+                    <li x-show="activeIndex === index">
+                        <a :href="" class="hover:text-primary text-gray-700" x-text="link.text"></a>
+                    </li>
+                </template>
+            </ul>
+
+            <div class="flex space-x-2">
+                <template x-for="(link, index) in links" :key="index">
+                    <div :class="activeIndex === index ? 'bg-black' : 'bg-gray-400'"
+                        class="w-3 h-3 rounded-full transition-all duration-300"></div>
+                </template>
+            </div>
+
+        </div>
 
     </nav>
     <main>
