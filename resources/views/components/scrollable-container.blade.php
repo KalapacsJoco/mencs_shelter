08f7841af1aff710b92347a6e72214f50d1095da
KalapacsJoco <kalapacs.joco79@gmail.com>
2025-02-05 20:25:07 +0200

    to check the alpine duplication

--- resources/views/components/scrollable-container.blade.php
+++ resources/views/components/scrollable-container.blade.php
@@ -1,22 +1,22 @@
 <div x-data="{ scrollPos: 0, maxScroll: 0 }"
      x-init="$nextTick(() => maxScroll = $refs.scrollContainer.scrollWidth - $refs.scrollContainer.clientWidth)"
-     class="relative w-2/3 mx-auto overflow-hidden">
+     class="relative w-2/3 mt-5">
 
     <button x-on:click="$refs.scrollContainer.scrollBy({ left: -500, behavior: 'smooth' })"
             x-show="scrollPos > 0"
-            class="absolute -left-5 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg">
+            class="hidden md:block absolute -left-20 top-1/2 transform -translate-y-1/2 bg-btnsecondary p-2 rounded-full shadow-lg">
         <x-swg.chevronleft-swg />
     </button>
 
     <div x-ref="scrollContainer"
          x-on:scroll="scrollPos = $event.target.scrollLeft"
-         class="flex space-x-4 overflow-x-auto scroll-smooth hide-scrollbar">
+         class="flex overflow-x-auto scroll-smooth hide-scrollbar">
         {{ $slot }} 
     </div>
 
     <button x-on:click="$refs.scrollContainer.scrollBy({ left: 500, behavior: 'smooth' })"
             x-show="scrollPos < maxScroll"
-            class="absolute -right-5 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg">
+            class="hidden md:block absolute -right-20 top-1/2 transform -translate-y-1/2 bg-btnsecondary p-2 rounded-full shadow-lg">
         <x-swg.chevronright-swg />
     </button>
 
--- resources/views/livewire/shelter.blade.php
+++ resources/views/livewire/shelter.blade.php
@@ -1,9 +1,9 @@
 <x-scrollable-container>
-    <h2 class="font-bold text-2xl mt-5">Shelters</h2>
     <div x-ref="scrollContainer" x-on:scroll="scrollPos = $event.target.scrollLeft"
-        class="flex space-x-4 overflow-x-auto scroll-smooth hide-scrollbar">
+        class="flex space-x-4 overflow-x-auto scroll-smooth">
+        {{-- <h2 class="font-bold text-2xl">Shelters</h2> --}}
         @foreach ($shelters as $shelter)
-        <section class="flex flex-col items-center justify-center drop-shadow-md border p-5 m-2 rounded-xl"
+        <section class="flex flex-col items-center justify-center drop-shadow-md border p-5 m-2 rounded-xl hover:bg-btnprimary"
             wire:key="shelter-{{ $shelter->id }}" wire:click='goToShelter({{ $shelter->id }})'>
             <img src="{{ asset('storage/' . $shelter->images->first()->path) }}" alt="Shelter image"
                 class="w-[256px] h-[173px] rounded-[12px] p-2">
