aef136b330070e4d21bf69e8156627dd665b3567
KalapacsJoco <kalapacs.joco79@gmail.com>
2025-02-06 19:37:31 +0200

    importing changes

--- resources/views/livewire/shelter.blade.php
+++ resources/views/livewire/shelter.blade.php
@@ -3,17 +3,26 @@
     <div x-ref="scrollContainer" x-on:scroll="scrollPos = $event.target.scrollLeft"
         class="flex space-x-4 overflow-x-auto scroll-smooth hide-scrollbar">
         @foreach ($shelters as $shelter)
-                <section 
-                        class="flex flex-col items-center justify-center drop-shadow-md border p-5 m-2 rounded-xl"
-                        wire:key="shelter-{{ $shelter->id }}" 
-                        wire:click='goToShelter({{ $shelter->id }})'>
-                    <img src="{{ asset('storage/' . $shelter->images->first()->path) }}" alt="Shelter image"
-                        class="w-[256px] h-[173px] rounded-[12px] p-2">
-                    <h3 class="w-[256px] font-bold text-2xl leading-[26.76px] p-2">{{ $shelter->name }}</h3>
-                    <article>
-                        {{ \Illuminate\Support\Str::limit($shelter->description, 100, '...') }}
-                    </article>
+        <a href="{{ route('shelters.show', $shelter->id) }}">
+            <section class="flex flex-col items-center justify-center drop-shadow-md border p-5 m-2 rounded-xl"
+                wire:key="shelter-{{ $shelter->id }}" wire:click='goToShelter({{ $shelter->id }})'>
+                <img src="{{ asset('storage/' . $shelter->images->first()->path) }}" alt="Shelter image"
+                    class="w-[256px] h-[173px] rounded-[12px] p-2">
+                <h3 class="w-[256px] font-bold text-2xl leading-[26.76px] p-2">{{ $shelter->name }}</h3>
+                <article>
+                    {{ \Illuminate\Support\Str::limit($shelter->description, 100, '...') }}
+                </article>
             </section>
+        </a>
         @endforeach
+
+        @if (count($shelters) < $totalShelters) <div class="flex justify-center items-center p-4">
+            <button 
+            wire:click="loadMore" 
+            class="primary-button-default hover:primary-button-hover">
+                Load More
+            </button>
+    </div>
+    @endif
     </div>
 </x-scrollable-container>
