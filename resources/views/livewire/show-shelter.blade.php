0bd06df1dd758ea354e9ab4941d09ab5a9827e98
KalapacsJoco <kalapacs.joco79@gmail.com>
2025-02-07 10:19:42 +0200

    before merge

--- resources/views/livewire/show-shelter.blade.php
+++ resources/views/livewire/show-shelter.blade.php
@@ -5,44 +5,44 @@
             <h3>{{$shelter->city}}</h3>
             <section class="md:flex gap-8 h-[60vh]">
                 <x-image-gallery :images="$shelter->images" />
-                    <aside class="w-full md:w-1/3">
-                    <section class="bg-background shadow-sm rounded-xl">
-                        <h2 class="h1-style mx-2 border-b-2">{{$shelter->name}}</h2>
-                        <div class="flex items-center space-x-2">
-                            <span>
-                                <x-swg.location-swg />
-                            </span>
-                            <address>Location: {{$shelter->city}}, {{$shelter->street}}</address>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <span>
-                                <x-swg.paws-swg />
-                            </span>
-                            <p>{{$shelter->animals->count()}} pet awaits their owner</p>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <span>
-                                <x-swg.heart-swg />
-                            </span>
-                            <p>Number of likes: {{$shelter->likes}}</p>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <span>
-                                <x-swg.phone-swg />
-                            </span>
-                            <p>Phone: {{$shelter->phone_number}}</p>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <span>
-                                <x-swg.mail-swg />
-                            </span>
-                            <p>Email: {{$shelter->email}}</p>
-                        </div>
-                    </section>
-                    <article class="border my-8 rounded-xl ">
-                        {{$shelter->description}}
-                    </article>
-                </aside>
+                    <div class="w-full md:w-1/3">
+                        <section class="bg-background shadow-sm rounded-xl">
+                            <h2 class="h1-style mx-2 border-b-2">{{$shelter->name}}</h2>
+                            <div class="flex items-center space-x-2">
+                                <span>
+                                    <x-swg.location-swg />
+                                </span>
+                                <span>Location: {{$shelter->city}}, {{$shelter->street}}</span>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <span>
+                                    <x-swg.paws-swg />
+                                </span>
+                                <p>{{$shelter->animals->count()}} pet awaits their owner</p>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <span>
+                                    <x-swg.heart-swg />
+                                </span>
+                                <p>Number of likes: {{$shelter->likes}}</p>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <span>
+                                    <x-swg.phone-swg />
+                                </span>
+                                <p>Phone: {{$shelter->phone_number}}</p>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <span>
+                                    <x-swg.mail-swg />
+                                </span>
+                                <p>Email: {{$shelter->email}}</p>
+                            </div>
+                        </section>
+                        <article class="border my-8 rounded-xl ">
+                            {{$shelter->description}}
+                        </article>
+                    </div>
             </section>
         </article>
     </x-app-layout>
